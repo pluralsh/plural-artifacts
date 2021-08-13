109a07e92a37eefe4991714dc07a1a97f5f6e359
DavidSpek <vanderspek.david@gmail.com>
2021-08-13 13:04:34 +0200

    Knative init

--- knative/terraform/kube/main.tf
+++ knative/terraform/kube/main.tf
@@ -3,6 +3,9 @@ resource "kubernetes_namespace" "knative" {
     name = var.namespace
     labels = {
       "app.kubernetes.io/managed-by" = "plural"
+      "serving.knative.dev/release" = "v0.24.0"
+      "istio-injection" = "enabled"
+      "app.plural.sh/name" = "knative"
     }
   }
 }
