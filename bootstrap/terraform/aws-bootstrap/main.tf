358a53c99ab473fdcd7fe4ddc5a986b0fdf0221c
DavidSpek <vanderspek.david@gmail.com>
2021-08-26 11:10:08 +0200

    Remove unused variable

--- bootstrap/terraform/aws-bootstrap/main.tf
+++ bootstrap/terraform/aws-bootstrap/main.tf
@@ -56,36 +56,27 @@ resource "aws_eks_addon" "vpc_cni" {
   cluster_name = var.cluster_name
   addon_name   = "vpc-cni"
   addon_version     = "v1.8.0-eksbuild.1"
-  tags = merge(
-    var.tags,
-    {
+  tags = {
       "eks_addon" = "vpc-cni"
-    }
-  )
+  }
 }
 
 resource "aws_eks_addon" "core_dns" {
   cluster_name      = var.cluster_name
   addon_name        = "coredns"
   addon_version     = "v1.8.3-eksbuild.1"
-  tags = merge(
-    var.tags,
-    {
+  tags = {
       "eks_addon" = "coredns"
-    }
-  )
+  }
 }
 
 resource "aws_eks_addon" "kube_proxy" {
   cluster_name      = var.cluster_name
   addon_name        = "kube-proxy"
   addon_version     = "v1.18.8-eksbuild.1"
-  tags = merge(
-    var.tags,
-    {
+  tags = {
       "eks_addon" = "kube-proxy"
-    }
-  )
+  }
 }
 
 resource "kubernetes_namespace" "bootstrap" {
