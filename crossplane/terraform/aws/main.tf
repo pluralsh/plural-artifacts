ae6df2273bf687b56d83d5967ac58fc1e8d01c1d
DavidSpek <vanderspek.david@gmail.com>
2021-09-28 12:00:57 +0200

    Set IAM policy on crossplane namespace

--- crossplane/terraform/aws/main.tf
+++ crossplane/terraform/aws/main.tf
@@ -19,7 +19,7 @@ module "assumable_role_crossplane" {
   role_name                     = "${var.cluster_name}-${var.role_name}"
   provider_url                  = replace(data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")
   role_policy_arns              = [aws_iam_policy.crossplane.arn]
-  oidc_subjects_with_wildcards = ["system:serviceaccount:${var.namespace}:provider-aws-*"]
+  oidc_subjects_with_wildcards = ["system:serviceaccount:${var.namespace}:*"]
 }
 
 resource "aws_iam_policy" "crossplane" {
