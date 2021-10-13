d4084149f7afc96bcb5fcffb9bb77e5d41528cd7
DavidSpek <vanderspek.david@gmail.com>
2021-10-13 20:40:17 +0200

    deployable

--- bootstrap/terraform/aws-efs/main.tf
+++ bootstrap/terraform/aws-efs/main.tf
@@ -2,9 +2,17 @@ data "aws_eks_cluster" "cluster" {
   name = var.cluster_name
 }
 
-data "aws_vpc" "cluster_vpcs" {
-  for_each = { for vpc in data.aws_eks_cluster.cluster.vpc_config : vpc.vpc_id => vpc }
-  id = each.value.vpc_id
+data "aws_vpc" "cluster_vpc" {
+  # for_each = { for vpc in data.aws_eks_cluster.cluster.vpc_config : vpc.vpc_id => vpc }
+  id = data.aws_eks_cluster.cluster.vpc_config[0].vpc_id
+}
+
+data "aws_subnet_ids" "cluster_private_subnets" {
+  vpc_id = data.aws_vpc.cluster_vpc.id
+  filter {
+    name   = "tag:Name"
+    values = ["${var.cluster_name}-private-*"]
+  }
 }
 
 locals {
@@ -82,27 +90,28 @@ data "aws_iam_policy_document" "efs_csi" {
 resource "aws_security_group" "allow_nfs" {
   name        = "allow_nfs-${var.cluster_name}"
   description = "Allow TLS inbound traffic"
+  vpc_id = data.aws_vpc.cluster_vpc.id
 
   # for_each = data.aws_eks_cluster.cluster.vpc_config
-  for_each = { for vpc in data.aws_eks_cluster.cluster.vpc_config : vpc.vpc_id => vpc }
+  # for_each = { for vpc in data.aws_eks_cluster.cluster.vpc_config : vpc.vpc_id => vpc }
 
   ingress {
       description      = "NFS from VPC"
       from_port        = 2049
       to_port          = 2049
       protocol         = "tcp"
-      cidr_blocks      = [cidrsubnet(data.aws_vpc.cluster_vpcs[each.key].cidr_block, 4, 1)]
-      security_groups = each.value.security_group_ids
+      cidr_blocks      = [data.aws_vpc.cluster_vpc.cidr_block]
+      # security_groups = data.aws_eks_cluster.cluster.vpc_config[0].security_group_ids
     }
   
 
   egress {
-      description      = "NFS from VPC"
+      description      = "NFS to VPC"
       from_port        = 2049
       to_port          = 2049
       protocol         = "tcp"
-      cidr_blocks      = [cidrsubnet(data.aws_vpc.cluster_vpcs[each.key].cidr_block, 4, 1)]
-      security_groups = each.value.security_group_ids
+      cidr_blocks      = [data.aws_vpc.cluster_vpc.cidr_block]
+      # security_groups = data.aws_eks_cluster.cluster.vpc_config[0].security_group_ids
     }
 }
 
@@ -114,8 +123,11 @@ resource "aws_efs_file_system" "efs_main" {
 
 resource "aws_efs_mount_target" "efs_mount_target" {
 
-  for_each = { for subnet_id in local.subnet_ids : "${subnet_id.vpc_key}.${subnet_id.subnet_key}" => subnet_id }
+  # for_each = { for subnet_id in local.subnet_ids : "${subnet_id.vpc_key}.${subnet_id.subnet_key}" => subnet_id }
+
+  for_each = data.aws_subnet_ids.cluster_private_subnets.ids
 
   file_system_id = aws_efs_file_system.efs_main.id
-  subnet_id      = each.value.subnet_id
+  subnet_id      = each.value
+  security_groups = [aws_security_group.allow_nfs.id]
 }
