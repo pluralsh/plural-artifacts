e61bccdbf7091197d8d4e9cedfce810f826ad7e2
michaelguarino <mguarino46@gmail.com>
2022-02-02 17:06:09 -0500

    use calendso head for img build

--- calendso/docker-entrypoint.sh
+++ calendso/docker-entrypoint.sh
@@ -12,7 +12,7 @@ export NEXT_PUBLIC_APP_URL=$BASE_URL
 function apply_app_url() {
     echo "Check that we have NEXT_PUBLIC_APP_URL vars"
     test -n "NEXT_PUBLIC_APP_URL"
-    find /home/cal/calendso/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_APP_URL_VAR#$NEXT_PUBLIC_APP_URL#g"
+    find /home/cal/calendso/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#https://dummy.example.com#$NEXT_PUBLIC_APP_URL#g"
 }
 apply_app_url
 
