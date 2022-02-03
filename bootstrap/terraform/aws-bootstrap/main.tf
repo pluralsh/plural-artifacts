9c98c6873fa25487c26c861016e3dd9e8cac4c9b
DavidSpek <vanderspek.david@gmail.com>
2022-02-03 15:22:27 +0100

    use new AWS VPC module

--- bootstrap/terraform/aws-bootstrap/main.tf
+++ bootstrap/terraform/aws-bootstrap/main.tf
@@ -3,15 +3,15 @@ data "aws_availability_zones" "available" {}
 data "aws_caller_identity" "current" {}
 
 module "vpc" {
-  source               = "terraform-aws-modules/vpc/aws"
-  version              = "2.78.0"
-  name                 = var.vpc_name
-  cidr                 = "10.0.0.0/16"
-  azs                  = data.aws_availability_zones.available.names
-  public_subnets       = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  enable_dns_hostnames = true
-  enable_ipv6 = true
+  source                 = "github.com/pluralsh/terraform-aws-vpc?ref=worker_subnet"
+  name                   = var.vpc_name
+  cidr                   = "10.0.0.0/16"
+  azs                    = data.aws_availability_zones.available.names
+  public_subnets         = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets        = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  worker_private_subnets = ["10.0.16.0/20", "10.0.32.0/20", "10.0.48.0/20"]
+  enable_dns_hostnames   = true
+  enable_ipv6            = true
 
   enable_nat_gateway = true
   single_nat_gateway = true
@@ -25,13 +25,18 @@ module "vpc" {
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
     "kubernetes.io/role/internal-elb" = "1"
   }
+
+  worker_private_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb" = "1"
+  }
 }
 
 module "cluster" {
   source          = "github.com/pluralsh/terraform-aws-eks?ref=asg-tags"
   cluster_name    = var.cluster_name
   cluster_version = "1.21"
-  subnets         = concat(module.vpc.public_subnets, module.vpc.private_subnets)
+  subnets         = concat(module.vpc.public_subnets_ids, module.vpc.private_subnets_ids)
   vpc_id          = module.vpc.vpc_id
   enable_irsa     = true
   write_kubeconfig = false
@@ -43,7 +48,7 @@ module "cluster" {
 
     instance_types = var.instance_types
     disk_size = 50
-    subnets = module.vpc.private_subnets
+    subnets = module.vpc.private_subnets_ids
     ami_release_version = "1.21.2-20210813"
     force_update_version = true
     ami_type = "AL2_x86_64"
@@ -108,22 +113,4 @@ resource "kubernetes_namespace" "bootstrap" {
   depends_on = [
     module.cluster.cluster_id
   ]
-}
-
-data "aws_subnet_ids" "cluster_private_subnets" {
-  vpc_id = module.vpc.vpc_id
-  filter {
-    name   = "tag:Name"
-    values = ["${var.vpc_name}-private-*"]
-  }
-  depends_on = [module.vpc]
-}
-
-data "aws_subnet_ids" "cluster_public_subnets" {
-  vpc_id = module.vpc.vpc_id
-  filter {
-    name   = "tag:Name"
-    values = ["${var.vpc_name}-public-*"]
-  }
-  depends_on = [module.vpc]
 }
