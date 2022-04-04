92a40879092254a4c9a5d46d1d9e1b053db84a88
michaelguarino <mguarino46@gmail.com>
2022-04-04 19:01:43 -0400

    clean up argo-workflows packaging

--- argo-workflows/terraform/aws/main.tf
+++ argo-workflows/terraform/aws/main.tf
@@ -43,6 +43,7 @@ data "aws_iam_policy_document" "argo_workflows" {
     actions = ["s3:ListAllMyBuckets", "s3:GetBucketLocation"]
     resources = ["arn:aws:s3:::*"]
   }
+
   statement {
     sid    = "admin"
     effect = "Allow"
