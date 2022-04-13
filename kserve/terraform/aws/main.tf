8e3a6aa0ee092235951b1f86b865fd2d880c4efa
DavidSpek <vanderspek.david@gmail.com>
2022-04-13 12:23:23 +0200

    fix istio injection

--- kserve/terraform/aws/main.tf
+++ kserve/terraform/aws/main.tf
@@ -4,6 +4,7 @@ resource "kubernetes_namespace" "kserve" {
     labels = {
       "app.kubernetes.io/managed-by" = "plural"
       "app.plural.sh/name" = "kserve"
+      "istio-injection" = "enabled"
 
     }
   }
