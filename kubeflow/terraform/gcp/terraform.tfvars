67d01c6148fb2962438c3b579e88d916be0f8b60
DavidSpek <vanderspek.david@gmail.com>
2022-04-25 13:00:51 +0200

    first pass pipelines
    Signed-off-by: DavidSpek <vanderspek.david@gmail.com>

--- kubeflow/terraform/gcp/terraform.tfvars
+++ kubeflow/terraform/gcp/terraform.tfvars
@@ -1,2 +1,5 @@
+project_id = {{ .Project | quote }}
+cluster_name = {{ .Cluster | quote }}
 namespace = {{ .Namespace | quote }}
 pipelines_bucket = {{ .Values.pipelines_bucket | quote }}
+bucket_location = {{ .Context.BucketLocation | quote }}
--- kubeflow/terraform/gcp/variables.tf
+++ kubeflow/terraform/gcp/variables.tf
@@ -6,3 +6,18 @@ variable "namespace" {
 variable "pipelines_bucket" {
   type = string
 }
+
+variable "bucket_location" {
+  type = string
+  default = "US"
+  description = "the location of the bucket"
+}
+
+variable "project_id" {
+  type = string
+  description = "The ID of the project in which the resources belong."
+}
+
+variable "cluster_name" {
+  type = string
+}
