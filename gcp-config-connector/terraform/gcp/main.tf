16ba10755f7c6347ad092a153fddd0f70963ac8d
DavidSpek <vanderspek.david@gmail.com>
2022-04-26 15:21:06 +0200

    add gcp-config-connector
    Signed-off-by: DavidSpek <vanderspek.david@gmail.com>

--- gcp-config-connector/terraform/gcp/main.tf
+++ gcp-config-connector/terraform/gcp/main.tf
@@ -1,3 +1,17 @@
+resource "kubernetes_namespace" "dummy" {
+  metadata {
+    name = "gcp-config-connector"
+    labels = {
+      "app.kubernetes.io/managed-by" = "plural"
+      "app.plural.sh/name" = "gcp-config-connector"
+      "cnrm.cloud.google.com/system" = "true"
+    }
+    annotations = {
+      "cnrm.cloud.google.com/version" = "1.82.0"
+    }
+  }
+}
+
 resource "kubernetes_namespace" "gcp-config-connector" {
   metadata {
     name = var.namespace
@@ -18,7 +32,7 @@ resource "kubernetes_service_account" "gcp-cc" {
     namespace = var.namespace
     annotations = {
       "cnrm.cloud.google.com/version" = "1.82.0"
-      "iam.gke.io/gcp-service-account" = module.gcp-cc-workload-identity.gcp_service_account_email
+      "iam.gke.io/gcp-service-account" = google_service_account.gcp-cc.email
     }
     labels = {
       "cnrm.cloud.google.com/system" = "true"
@@ -30,13 +44,49 @@ resource "kubernetes_service_account" "gcp-cc" {
   ]
 }
 
-module "gcp-cc-workload-identity" {
-  source     = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
-  name       = "${var.cluster_name}-gcp-config-connector"
-  namespace  = var.namespace
-  project_id = var.project_id
-  use_existing_k8s_sa = true
-  annotate_k8s_sa = false
-  k8s_sa_name = "cnrm-controller-manager"
-  roles = ["roles/owner"]
+resource "google_service_account" "gcp-cc" {
+  account_id   = "${var.cluster_name}-gcp-cc"
+  display_name = "Plural GCP Config Connector"
+}
+
+resource "google_service_account_iam_member" "gcp-cc" {
+  service_account_id = google_service_account.gcp-cc.name
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "serviceAccount:${var.project_id}.svc.id.goog[${var.namespace}/cnrm-controller-manager]"
+}
+
+resource "google_project_iam_member" "gcp-cc" {
+  role    = "roles/owner"
+  member  = "serviceAccount:${google_service_account.gcp-cc.email}"
+  project = var.project_id
+}
+
+# module "gcp-cc-workload-identity" {
+#   source     = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+#   name       = "${var.cluster_name}-gcp-config-connector"
+#   namespace  = var.namespace
+#   project_id = var.project_id
+#   use_existing_k8s_sa = true
+#   annotate_k8s_sa = false
+#   k8s_sa_name = "cnrm-controller-manager"
+#   roles = ["roles/owner"]
+# }
+
+resource "google_service_account_key" "gcp_config_connector_key" {
+  service_account_id = google_service_account.gcp-cc.name
+}
+
+resource "kubernetes_secret" "google-application-credentials" {
+  metadata {
+    name = "gcp-config-connector-credentials"
+    namespace = var.namespace
+  }
+
+  data = {
+    "key.json" = base64decode(google_service_account_key.gcp_config_connector_key.private_key)
+  }
+
+  depends_on = [
+    kubernetes_namespace.gcp-config-connector
+  ]
 }
--- gcp-config-connector/terraform/gcp/variables.tf
+++ gcp-config-connector/terraform/gcp/variables.tf
@@ -1,8 +1,13 @@
 variable "namespace" {
   type = string
-  default = "gcp-config-connector"
+  default = "cnrm-system"
 }
 
 variable "cluster_name" {
   type = string
+}
+
+variable "project_id" {
+  type = string
+  description = "The ID of the project in which the resources belong."
 }
