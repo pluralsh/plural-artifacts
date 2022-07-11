8842745c5bed5f77fcf03a05801813ca60521d98
michaeljguarino <mguarino46@gmail.com>
2022-07-11 09:55:16 -0400

    Onboard datadog (#319)
    We need this to help activate draftkings

---
+++ datadog/plural/notes.tpl
@@ -0,0 +1,1 @@
+Use `plural watch datadog` to track the status of your application
---
+++ datadog/terraform/kube/variables.tf
@@ -0,0 +1,8 @@
+variable "namespace" {
+  type = string
+  default = "datadog"
+}
+
+variable "cluster_name" {
+  type = string
+}
