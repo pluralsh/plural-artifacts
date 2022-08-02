78f9d0c5f53e986ee345f40337d0f3c403280f11
michaeljguarino <mguarino46@gmail.com>
2022-08-01 23:56:08 -0400

    Onboard mongodb (#339)
    * Onboard mongodb
    
    Just create a simple mongo cluster and initialize the operator
    
    * Onboard growthbook (#340)
    
    Onboards growthbook using our new setup for mongodb.  Still need to hammer on it to test some more but should be pretty straightforward.
    
    * fix azure setup

---
+++ growthbook/terraform/aws/main.tf
@@ -0,0 +1,31 @@
+resource "kubernetes_namespace" "growthbook" {
+  metadata {
+    name = var.namespace
+    labels = {
+      "app.kubernetes.io/managed-by" = "plural"
+      "app.plural.sh/name" = "growthbook"
+    }
+  }
+}
+
+module "s3_buckets" {
+  source = "github.com/pluralsh/module-library//terraform/s3-buckets"
+  bucket_names = [var.dagster_bucket]
+  policy_prefix = "growthbook"
+}
+
+module "assumable_role_growthbook" {
+  source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version                       = "3.14.0"
+  create_role                   = true
+  role_name                     = "${var.cluster_name}-${var.role_name}"
+  provider_url                  = replace(data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")
+  role_policy_arns              = [module.s3_buckets.policy_arn]
+  oidc_subjects_with_wildcards = [
+    "system:serviceaccount:${var.namespace}:growthbook",
+  ]
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_name
+}
