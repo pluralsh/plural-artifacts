1aaed9c6fc80d36e43d37df3bdc0e73fe29e32f8
EXTERNAL Antoniu Bogdan (Fortech, EB-DB/ENG-Ku) <external.Bogdan.Antoniu@ro.bosch.com>
2022-08-03 17:44:16 +0300

    initial commit

---
+++ datahub/terraform/aws/main.tf
@@ -0,0 +1,32 @@
+resource "kubernetes_namespace" "datahub" {
+  metadata {
+    name = var.namespace
+    labels = {
+      "app.kubernetes.io/managed-by" = "plural"
+      "app.plural.sh/name" = "datahub"
+
+      "platform.plural.sh/sync-target" = "pg"
+
+    }
+  }
+}
+
+
+data "aws_iam_role" "postgres" {
+  name = "${var.cluster_name}-postgres"
+}
+
+resource "kubernetes_service_account" "postgres" {
+  metadata {
+    name      = "postgres-pod"
+    namespace = var.namespace
+
+    annotations = {
+      "eks.amazonaws.com/role-arn" = data.aws_iam_role.postgres.arn
+    }
+  }
+
+  depends_on = [
+    kubernetes_namespace.datahub
+  ]
+}
---
+++ datahub/terraform/aws/variables.tf
@@ -0,0 +1,8 @@
+variable "namespace" {
+  type = string
+  default = "datahub"
+}
+
+variable "cluster_name" {
+  type = string
+}
