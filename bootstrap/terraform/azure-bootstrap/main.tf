664564a84955e41e4bb9d1d6239a2fd0f78848ba
David van der Spek <vanderspek.david@gmail.com>
2022-09-15 07:51:27 +0200

    Azure bootstrap improvements v2 (#318)
    * network name
    
    Signed-off-by: DavidSpek <vanderspek.david@gmail.com>
    
    * subnet prefix and module update. requires provider 3.3
    
    Signed-off-by: DavidSpek <vanderspek.david@gmail.com>
    
    * new node groups
    
    Signed-off-by: DavidSpek <vanderspek.david@gmail.com>
    
    * fix templating for new deployments
    
    Signed-off-by: DavidSpek <vanderspek.david@gmail.com>
    
    * add providerVsn to dependency
    
    Signed-off-by: DavidSpek <vanderspek.david@gmail.com>
    
    * add spot node groups
    
    Signed-off-by: DavidSpek <vanderspek.david@gmail.com>
    
    * make network plugin and policy configurable
    
    Signed-off-by: DavidSpek <vanderspek.david@gmail.com>
    
    * fix provider_vsn
    
    Signed-off-by: DavidSpek <vanderspek.david@gmail.com>
    
    * split node groups by AZ
    
    Signed-off-by: DavidSpek <vanderspek.david@gmail.com>
    
    * set 3 base node groups as system
    
    Signed-off-by: DavidSpek <vanderspek.david@gmail.com>
    
    * fix setting base groups as system
    
    Signed-off-by: DavidSpek <vanderspek.david@gmail.com>
    
    * cleanup variables code
    
    Signed-off-by: DavidSpek <vanderspek.david@gmail.com>
    
    Signed-off-by: DavidSpek <vanderspek.david@gmail.com>

--- bootstrap/terraform/azure-bootstrap/main.tf
+++ bootstrap/terraform/azure-bootstrap/main.tf
@@ -4,6 +4,8 @@ data "azurerm_resource_group" "group" {
 
 module "network" {
   source              = "github.com/pluralsh/terraform-azurerm-network?ref=plural"
+
+  vnet_name           = var.network_name
   resource_group_name = data.azurerm_resource_group.group.name
   address_space       = var.address_space
   subnet_prefixes     = var.subnet_prefixes
@@ -11,47 +13,71 @@ module "network" {
 }
 
 module "aks" {
-  source                           = "github.com/pluralsh/terraform-azurerm-aks?ref=plural"
+  source                           = "github.com/pluralsh/terraform-azurerm-aks?ref=plural-updated"
   resource_group_name              = data.azurerm_resource_group.group.name
   kubernetes_version               = var.kubernetes_version
   orchestrator_version             = var.kubernetes_version
   prefix                           = var.name
   cluster_name                     = var.name
-  network_plugin                   = "azure"
+  network_plugin                   = var.network_plugin
   vnet_subnet_id                   = module.network.vnet_subnets[0]
-  os_disk_size_gb                  = var.os_disk_size
-  os_disk_type                     = var.os_disk_type
+  os_disk_size_gb                  = var.node_groups[0].os_disk_size_gb
+  os_disk_type                     = var.node_groups[0].os_disk_type
   enable_role_based_access_control = true
   rbac_aad_enabled                 = false 
   rbac_aad_managed                 = false
   sku_tier                         = "Paid"
   private_cluster_enabled          = var.private_cluster
   enable_http_application_routing  = true
-  enable_azure_policy              = true
-  enable_auto_scaling              = true
-  agents_min_count                 = var.min_nodes
-  agents_max_count                 = var.max_nodes
-  agents_count                     = null # Please set `agents_count` `null` while `enable_auto_scaling` is `true` to avoid possible `agents_count` changes.
-  agents_max_pods                  = 100
-  agents_pool_name                 = local.node_pool_name
-  agents_availability_zones        = ["1", "2"]
+  azure_policy_enabled             = true
+  admin_username                   = var.admin_username
+  enable_auto_scaling              = var.node_groups[0].enable_auto_scaling
+  agents_min_count                 = var.node_groups[0].min_count
+  agents_max_count                 = var.node_groups[0].max_count
+  agents_count                     = var.node_groups[0].node_count  # Please set `agents_count` `null` while `enable_auto_scaling` is `true` to avoid possible `agents_count` changes.
+  agents_max_pods                  = var.node_groups[0].max_pods
+  agents_pool_name                 = var.node_groups[0].name
+  agents_availability_zones        = var.node_groups[0].availability_zones
   agents_type                      = "VirtualMachineScaleSets"
-  agents_size                      = var.agents_size
+  agents_size                      = var.node_groups[0].vm_size
 
-  agents_labels = {
-    "nodepool" : local.node_pool_name
-  }
+  agents_labels = var.node_groups[0].node_labels
 
-  agents_tags = {
-    "Agent" : "${local.node_pool_name}agent"
-  }
+  agents_tags = var.node_groups[0].tags
 
-  network_policy                 = "azure"
+  network_policy                 = var.network_policy
   net_profile_dns_service_ip     = "10.0.0.10"
   net_profile_docker_bridge_cidr = "170.10.0.1/16"
   net_profile_service_cidr       = "10.0.0.0/16"
 
   depends_on = [module.network]
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "main" {
+  for_each = {for idx, val in var.node_groups : val.name => val if idx != 0}
+
+  kubernetes_cluster_id = module.aks.aks_id
+
+  name                  = each.value.name
+  priority              = each.value.priority
+  enable_auto_scaling   = each.value.enable_auto_scaling
+  zones                 = each.value.availability_zones
+  mode                  = each.value.mode
+  orchestrator_version  = var.kubernetes_version
+  node_count            = each.value.node_count
+  min_count             = each.value.min_count
+  max_count             = each.value.max_count
+  spot_max_price        = each.value.spot_max_price
+  eviction_policy       = each.value.eviction_policy
+  vnet_subnet_id        = module.network.vnet_subnets[0]
+  vm_size               = each.value.vm_size
+  os_disk_type          = each.value.os_disk_type
+  os_disk_size_gb       = each.value.os_disk_size_gb
+  max_pods              = each.value.max_pods
+
+  node_labels           = each.value.node_labels
+  node_taints           = each.value.node_taints
+  tags                  = each.value.tags
 }
 
 data "azurerm_resource_group" "node_group" {
