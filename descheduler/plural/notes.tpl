699f16969c318da793896206e16c243537ba4df6
DavidSpek <vanderspek.david@gmail.com>
2022-09-29 16:59:02 +0200

    init for testing
    Signed-off-by: DavidSpek <vanderspek.david@gmail.com>

---
+++ descheduler/plural/notes.tpl
@@ -0,0 +1,1 @@
+Use `plural watch descheduler` to track the status of your application
---
+++ descheduler/terraform/azure/variables.tf
@@ -0,0 +1,8 @@
+variable "namespace" {
+  type = string
+  default = "descheduler"
+}
+
+variable "cluster_name" {
+  type = string
+}
