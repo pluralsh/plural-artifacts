a20b29e2abc8d98ef465e24e83c001e791d7f563
Nico Becker <Nico.Becker@alexanderthamm.com>
2022-10-24 19:17:09 +0200

    select subnet route table for s3 vpc endpoint

--- bootstrap/terraform/aws-bootstrap/s3-vpc-endpoint.tf
+++ bootstrap/terraform/aws-bootstrap/s3-vpc-endpoint.tf
@@ -1,6 +1,10 @@
+data "aws_route_table" "worker_private_subnets_route_table" {
+  subnet_id = module.vpc.worker_private_subnets_ids[0]
+}
+
 resource "aws_vpc_endpoint" "s3" {
   vpc_id       = module.vpc.vpc_id
   service_name = "com.amazonaws.${var.aws_region}.s3"
   auto_accept = true
-  route_table_ids = [module.vpc.default_route_table_id]
+  route_table_ids = [data.aws_route_table.worker_private_subnets_route_table.id]
 }
