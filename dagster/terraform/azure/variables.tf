edc24a05255f427ff8becf75bd2ab8cd2380310b
michaeljguarino <mguarino46@gmail.com>
2022-11-03 23:43:35 -0400

    Backfill GCP and Azure support for dagster
    It was brought to my attention we never rounded out gcp and azure for dagster, which is a tad embarrassing.  This should handle it, the manin issue is getting object storage working.

--- dagster/terraform/azure/variables.tf
+++ dagster/terraform/azure/variables.tf
@@ -2,3 +2,22 @@ variable "namespace" {
   type = string
   default = "dagster"
 }
+
+variable "dagster_bucket" {
+  type = string
+}
+
+variable "minio_namespace" {
+  type = string
+  default = "minio"
+}
+
+variable "minio_region" {
+  description = "Default MinIO region"
+  default     = "us-east-1"
+}
+
+variable "minio_server" {
+  description = "Default MinIO host and port"
+  default = "minio.minio:9000"
+}
--- dagster/terraform/gcp/variables.tf
+++ dagster/terraform/gcp/variables.tf
@@ -2,3 +2,7 @@ variable "namespace" {
   type = string
   default = "dagster"
 }
+
+variable "dagster_bucket" {
+  type = string
+}
