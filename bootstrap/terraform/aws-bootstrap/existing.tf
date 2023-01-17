eeb444b446c17ce90fdc4aef875ea870d6e0e67b
David van der Spek <vanderspek.david@gmail.com>
2023-01-17 15:23:02 +0100

    output worker private subnets
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>

--- bootstrap/terraform/aws-bootstrap/existing.tf
+++ bootstrap/terraform/aws-bootstrap/existing.tf
@@ -7,3 +7,8 @@ data "aws_vpc" "vpc" {
     count = var.create_cluster ? 0 : 1
     id = local.vpc_id
 }
+
+data "aws_subnet" "worker_private_subnets" {
+    for_each = toset(var.worker_private_subnet_ids)
+    id       = each.value
+}
--- bootstrap/terraform/aws-bootstrap/locals.tf
+++ bootstrap/terraform/aws-bootstrap/locals.tf
@@ -2,8 +2,12 @@ locals {
     sts_principal = "sts.${data.aws_partition.current.dns_suffix}"
 
     private_subnet_ids = var.create_cluster ? module.vpc.private_subnets_ids : var.private_subnet_ids
+
     public_subnet_ids = var.create_cluster ? module.vpc.public_subnets_ids : var.public_subnet_ids
+
     worker_subnet_ids = var.create_cluster ? module.vpc.worker_private_subnets_ids : var.worker_private_subnet_ids
+    worker_private_subnets = var.create_cluster ? module.vpc.worker_private_subnets : data.aws_subnet.worker_private_subnets[*]
+
     vpc_id = var.create_cluster ? module.vpc.vpc_id : data.aws_eks_cluster.cluster[0].vpc_config[0].vpc_id
     
     cluster_id = var.create_cluster ? module.cluster.cluster_id : data.aws_eks_cluster.cluster[0].id
