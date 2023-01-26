4e9a49599b0f2f4a56fc40860c24c4e69aaa57de
David van der Spek <vanderspek.david@gmail.com>
2023-01-26 13:05:38 +0100

    encrypt airbyte s3 bucket
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>

--- airbyte/terraform/aws/variables.tf
+++ airbyte/terraform/aws/variables.tf
@@ -12,3 +12,9 @@ variable "airbyte_bucket" {
   type = string
   default = "airbyte"
 }
+
+variable "force_destroy_bucket" {
+  type        = bool
+  default     = true
+  description = "If true, the bucket will be deleted even if it contains objects."
+}
