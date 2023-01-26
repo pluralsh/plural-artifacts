ba9fe5e55bf021161cdf8e512805b543d93c270f
David van der Spek <vanderspek.david@gmail.com>
2023-01-26 13:07:37 +0100

    encrypt gitlab s3 bucket
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>

--- gitlab/terraform/aws/variables.tf
+++ gitlab/terraform/aws/variables.tf
@@ -67,3 +67,57 @@ variable "runner_role_name" {
   default = "gitlab-runner"
   description = "name of the IAM role for gitlab to assume"
 }
+
+variable "force_destroy_registry_bucket" {
+  type        = bool
+  default     = true
+  description = "If true, the registry bucket will be deleted even if it contains objects."
+}
+
+variable "force_destroy_lfs_bucket" {
+  type        = bool
+  default     = true
+  description = "If true, the lfs bucket will be deleted even if it contains objects."
+}
+
+variable "force_destroy_artifacts_bucket" {
+  type        = bool
+  default     = true
+  description = "If true, the artifacts bucket will be deleted even if it contains objects."
+}
+
+variable "force_destroy_packages_bucket" {
+  type        = bool
+  default     = true
+  description = "If true, the packages bucket will be deleted even if it contains objects."
+}
+
+variable "force_destroy_backups_bucket" {
+  type        = bool
+  default     = true
+  description = "If true, the backups bucket will be deleted even if it contains objects."
+}
+
+variable "force_destroy_backups_tmp_bucket" {
+  type        = bool
+  default     = true
+  description = "If true, the backups tmp bucket will be deleted even if it contains objects."
+}
+
+variable "force_destroy_uploads_bucket" {
+  type        = bool
+  default     = true
+  description = "If true, the uploads bucket will be deleted even if it contains objects."
+}
+
+variable "force_destroy_terraform_state_bucket" {
+  type        = bool
+  default     = true
+  description = "If true, the terraform state bucket will be deleted even if it contains objects."
+}
+
+variable "force_destroy_runner_cache_bucket" {
+  type        = bool
+  default     = true
+  description = "If true, the runner cache bucket will be deleted even if it contains objects."
+}
