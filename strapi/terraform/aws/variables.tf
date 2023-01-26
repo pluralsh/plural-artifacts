2acbe4de5f0f6da0fb58a4f756c2d0b48dec26c5
David van der Spek <vanderspek.david@gmail.com>
2023-01-26 13:48:50 +0100

    encrypt strapi s3 bucket
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>

--- strapi/terraform/aws/variables.tf
+++ strapi/terraform/aws/variables.tf
@@ -20,3 +20,9 @@ variable "serviceaccount_name" {
   type = string
   default = "strapi"
 }
+
+variable "force_destroy_bucket" {
+  type        = bool
+  default     = true
+  description = "If true, the bucket will be deleted even if it contains objects."
+}
