46b7dc8136c6e5eb7cb7ba409aa23902bb7d3e50
michaeljguarino <mguarino46@gmail.com>
2023-01-25 21:36:48 -0500

    Make monitoring namespace creation optional
    this is needed to help support byok, since xendit (and likely others) precreate a monitoring namespace

--- monitoring/terraform/aws/variables.tf
+++ monitoring/terraform/aws/variables.tf
@@ -2,3 +2,9 @@ variable "namespace" {
   type = string
   default = "monitoring"
 }
+
+variable "create" {
+  type = bool
+  default = true
+  description = "whether you want to create the monitoring namespace"
+}
