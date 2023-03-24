7dd169ac31517aa649f78f3833db649b86de8ca4
Walid <walid.elbouchikhi@beamy.io>
2023-03-24 17:06:55 +0100

    feat: sftpgo workload identity + fix sftpgo ui + add loadbalancer for TCP services (#572)
    * fix: fix sftpgo gcp depeendency
    
    * fix: fix sftpgo ingress + added optional service to access tcp services through a domain
    
    * feat(wip): add sftpgo workload identity
    
    * feat: add variables to register roles for sftpgo workload identity sa
    
    * fix: fix serviceAccount annotation
    
    * feat: bump sftpgo helm chart version

---
+++ sftpgo/terraform/gcp/main.tf
@@ -0,0 +1,20 @@
+resource "kubernetes_namespace" "sftpgo" {
+  metadata {
+    name = var.namespace
+    labels = {
+      "app.kubernetes.io/managed-by" = "plural"
+      "app.plural.sh/name" = "sftpgo"
+    }
+  }
+}
+
+module "sftpgo-workload-identity" {
+  source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+  name                = "${var.cluster_name}-sftpgo-workload"
+  namespace           = var.namespace
+  project_id          = var.project_id
+  use_existing_k8s_sa = true
+  annotate_k8s_sa     = false
+  k8s_sa_name         = "sftpgo"
+  roles               = var.roles
+}
