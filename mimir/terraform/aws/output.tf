943bfd595d1c85a208bdf31eaa23d6669d21da6b
David van der Spek <vanderspek.david@gmail.com>
2023-03-29 13:33:33 +0200

    fix output for the old version of module we use
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>

--- mimir/terraform/aws/output.tf
+++ mimir/terraform/aws/output.tf
@@ -1,4 +1,4 @@
 output "iam_role_arn" {
   description = "ARN of IAM role that allows access to the Mimir S3 buckets."
-  value       = module.assumable_role_mimir.iam_role_arn
+  value       = module.assumable_role_mimir.this_iam_role_arn
 }
