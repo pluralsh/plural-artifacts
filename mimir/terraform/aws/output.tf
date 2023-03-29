6820f5bb7d8530243d3fe2caf1689d40207e1c92
David van der Spek <vanderspek.david@gmail.com>
2023-03-29 13:33:33 +0200

    fix output
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>

--- mimir/terraform/aws/output.tf
+++ mimir/terraform/aws/output.tf
@@ -1,4 +1,4 @@
 output "iam_role_arn" {
   description = "ARN of IAM role that allows access to the Mimir S3 buckets."
-  value       = module.iam_assumable_role_admin.iam_role_arn
+  value       = module.assumable_role_mimir.iam_role_arn
 }
