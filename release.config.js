d9d9c0322c5561b6c9b248989fc7a114c3953458
David van der Spek <vanderspek.david@gmail.com>
2023-04-05 19:54:41 +0200

    ci: setup semantic release for applications (#624)

---
+++ release.config.js
@@ -0,0 +1,14 @@
+const name = process.env.APP_NAME;
+
+module.exports = {
+  branches: ["main"],
+  tagFormat: name + '-v${version}',
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github"
+  ],
+  commitPaths: [
+    name,
+  ]
+};
