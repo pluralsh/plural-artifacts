0795c855593f76731b410f2f81894720e60dc473
David van der Spek <vanderspek.david@gmail.com>
2023-05-30 19:23:14 +0200

    fix(mysql): Upgrade MySQL operator (#700)
    * fix(mysql): init update operator and helm chart
    
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>
    
    * add some notes
    
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>
    
    * fix: cleanup oidc login
    
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>
    
    * fix: don't mark aws percona recipe primary
    
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>
    
    * feat: add output for s3 iam role arn
    
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>
    
    * bump chart version
    
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>
    
    * set tf dependency for aws
    
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>
    
    * init add gcs support
    
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>
    
    * add sa email output
    
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>
    
    * fix sa email output
    
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>
    
    * cleanup
    
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>
    
    * set tf version for gcp
    
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>
    
    ---------
    
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>

--- mysql/terraform/gcp/terraform.tfvars
+++ mysql/terraform/gcp/terraform.tfvars
@@ -1,2 +1,5 @@
 cluster_name = {{ .Cluster | quote }}
 namespace = {{ .Namespace | quote }}
+project_id = {{ .Project | quote }}
+backup_bucket = {{ .Values.backup_bucket | quote }}
+bucket_location = {{ .Context.BucketLocation | quote }}
--- mysql/terraform/gcp/variables.tf
+++ mysql/terraform/gcp/variables.tf
@@ -8,3 +8,20 @@ variable "namespace" {
   default = "mysql"
 }
 
+variable "project_id" {
+  type = string
+  description = "The ID of the project in which the resources belong."
+}
+
+variable "mysql_serviceaccount" {
+  type = string
+  default = "mysql-operator"
+}
+
+variable "backup_bucket" {
+  type = string
+}
+
+variable "bucket_location" {
+  type = string
+}
