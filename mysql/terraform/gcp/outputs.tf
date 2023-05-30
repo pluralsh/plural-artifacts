76baebfd6ac12e1057732432d17f2a1132419477
David van der Spek <vanderspek.david@gmail.com>
2023-05-30 18:35:00 +0200

    fix sa email output
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>

--- mysql/terraform/gcp/outputs.tf
+++ mysql/terraform/gcp/outputs.tf
@@ -10,5 +10,5 @@ output "gcp_sa_name" {
 
 output "service_account_email" {
   description = "service account email that has access to the MySQL backup GCS buckets."
-  value       = module.harbor-workload-identity.gcp_service_account_email
+  value       = module.mysql-workload-identity.gcp_service_account_email
 }
