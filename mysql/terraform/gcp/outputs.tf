3f6599d710ead5f6383ad4de9b36e6291a49a158
David van der Spek <vanderspek.david@gmail.com>
2023-05-30 18:34:15 +0200

    add sa email output
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>

--- mysql/terraform/gcp/outputs.tf
+++ mysql/terraform/gcp/outputs.tf
@@ -7,3 +7,8 @@ output "gcp_sa_name" {
   description = "Name of the GCP service account that has access to the MySQL backup GCS bucket."
   value       = module.mysql-workload-identity.gcp_service_account_name
 }
+
+output "service_account_email" {
+  description = "service account email that has access to the MySQL backup GCS buckets."
+  value       = module.harbor-workload-identity.gcp_service_account_email
+}
