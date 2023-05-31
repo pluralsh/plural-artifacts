3a092abc7641f1238bace6d3ae8f48e63ec0366e
Sebastian Florek <s.florek91@gmail.com>
2023-05-31 10:29:16 +0200

    Update CAPI-GCP provider

--- bootstrap/terraform/gcp-bootstrap-cluster-api/main.tf
+++ bootstrap/terraform/gcp-bootstrap-cluster-api/main.tf
@@ -30,24 +30,11 @@ resource "kubernetes_service_account_v1" "externaldns" {
 module "certmanager-workload-identity" {
   source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
   use_existing_k8s_sa = true
-  annotate_k8s_sa     = true
   cluster_name        = var.cluster_name
   location            = var.gcp_region
-  name                = "certmanager"
+  name                = "${var.cluster_name}-certmanager"
   namespace           = var.namespace
+  k8s_sa_name         = "certmanager"
   project_id          = var.gcp_project_id
   roles               = ["roles/dns.admin"]
-
-  depends_on = [data.google_container_cluster.cluster]
 }
-
-#resource "kubernetes_service_account_v1" "certmanager" {
-#  metadata {
-#    name      = "certmanager"
-#    namespace = var.namespace
-#
-#    annotations = {
-#      "iam.gke.io/gcp-service-account" = module.certmanager-workload-identity.gcp_service_account_email
-#    }
-#  }
-#}
--- bootstrap/terraform/gcp-bootstrap-cluster-api/terraform.tfvars
+++ bootstrap/terraform/gcp-bootstrap-cluster-api/terraform.tfvars
@@ -1,3 +1,4 @@
 {{- $tfOutput := pathJoin repoRoot "bootstrap" "output.yaml" }}
 gcp_project_id = {{ .Project | quote }}
 gcp_region = {{ .Region | quote }}
+cluster_name = {{ .Cluster | quote }}
