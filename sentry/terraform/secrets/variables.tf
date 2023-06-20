39e3194c6818b3bea548719445475f1b20680af6
Hans Rauer <h.w.rauer@googlemail.com>
2023-06-20 17:14:16 +0200

    revert rabbitmq changes

--- sentry/terraform/secrets/variables.tf
+++ sentry/terraform/secrets/variables.tf
@@ -1,19 +1,19 @@
 variable "rabbitmq_namespace" {
-  type = string
+  type    = string
   default = "rabbitmq"
 }
 
 variable "rabbitmq_secret" {
-  type = string
+  type    = string
   default = "rabbitmq-default-user"
 }
 
 variable "redis_namespace" {
-  type = string
+  type    = string
   default = "redis"
 }
 
 variable "redis_secret" {
-  type = string
+  type    = string
   default = "redis-password"
 }
