292fad500ce3e9e4aa642b08cf6282f74af3dea6
michaeljguarino <mguarino46@gmail.com>
2023-06-30 03:59:11 -0400

    feat(directus): Add s3 support on AWS (#755)
    Needed to persist files durably

--- directus/terraform/aws/main.tf
+++ directus/terraform/aws/main.tf
@@ -9,6 +9,26 @@ resource "kubernetes_namespace" "directus" {
   }
 }
 
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_name
+}
+
+module "s3_buckets" {
+  source        = "github.com/pluralsh/module-library//terraform/s3-buckets?ref=bucket-protection"
+  bucket_names  = [var.directus_bucket]
+  policy_prefix = "directus"
+  force_destroy = var.force_destroy_bucket
+}
+
+module "assumable_role_directus" {
+  source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version                       = "3.14.0"
+  create_role                   = true
+  role_name                     = "${var.cluster_name}-directus"
+  provider_url                  = replace(data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")
+  role_policy_arns              = [module.s3_buckets.policy_arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:${var.namespace}:${var.serviceaccount_name}"]
+}
 
 data "aws_iam_role" "postgres" {
   name = "${var.cluster_name}-postgres"
--- directus/terraform/aws/variables.tf
+++ directus/terraform/aws/variables.tf
@@ -6,3 +6,17 @@ variable "namespace" {
 variable "cluster_name" {
   type = string
 }
+
+variable "directus_bucket" {
+  type = string
+}
+
+variable "force_destroy_bucket" {
+  type = bool
+  default = true
+}
+
+variable "serviceaccount_name" {
+  type = string
+  default = "directus"
+}
