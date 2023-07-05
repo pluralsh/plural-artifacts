c912e5c82c557f2383913c55ace5685892908d1d
David van der Spek <vanderspek.david@gmail.com>
2023-07-05 17:19:38 +0200

    Revert "ci: update renovate config file (#761)"
    This reverts commit 79c4d532eb2214878d2288d91c2e0b894b7fba6d.

--- bin/artifact-renovate-script.sh
+++ bin/artifact-renovate-script.sh
@@ -29,15 +29,23 @@ default='[
         "description": "Bump helm chart versions",
         "matchManagers": ["helmv3"],
         "bumpVersion": "patch"
+    },
+    {
+        "description": "Group Image Vendor updates",
+        "matchManagers": ["regex"],
+        "groupName": "vendor-images",
+        "additionalBranchPrefix": ""
     }
 ]'
+
+app="test"
 
 base=$(jq ".packageRules |= ${default}" renovate.json)
 sum=$(echo $base)
 
 for dir in $(echo */ | sed 's/\///g'); do
 
-    sum=$(echo $sum | jq ".packageRules |= .+ [{\"description\": \"Group $dir Helm updates\", \"matchFileNames\": [\"$dir/**\"], \"matchManagers\": [\"helm-requirements\", \"helm-values\", \"helmfile\", \"helmsman\", \"helmv3\"], \"groupName\": \"$dir\", \"additionalBranchPrefix\": \"\"}]")
+    sum=$(echo $sum | jq ".packageRules |= .+ [{\"description\": \"Group $dir Helm updates\", \"matchPaths\": [\"$dir\"], \"matchManagers\": [\"helm-requirements\", \"helm-values\", \"helmfile\", \"helmsman\", \"helmv3\"], \"groupName\": \"$dir\", \"additionalBranchPrefix\": \"\"}]")
 
 done
 echo $sum | jq > renovate.json
