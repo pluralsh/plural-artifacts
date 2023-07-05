c4b9fb1a937a01206e4e448e68414323f932fd94
David van der Spek <vanderspek.david@gmail.com>
2023-07-05 18:35:44 +0200

    ci: improve renovate config a bit more
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>

--- bin/artifact-renovate-script.sh
+++ bin/artifact-renovate-script.sh
@@ -29,12 +29,6 @@ default='[
         "description": "Bump helm chart versions",
         "matchManagers": ["helmv3"],
         "bumpVersion": "patch"
-    },
-    {
-        "description": "Set scope of PRs",
-        "matchManagers": ["helmv3", "helmfile", "helm-requirements", "helm-values", "helmsman"],
-        "semanticCommitScope": "{{baseDir}}",
-        "semanticCommitType": "{{#if isPatch}}fix{{else}}feat{{/if}}"
     }
 ]'
 
@@ -43,7 +37,7 @@ sum=$(echo $base)
 
 for dir in $(echo */ | sed 's/\///g'); do
 
-    sum=$(echo $sum | jq ".packageRules |= .+ [{\"description\": \"Group $dir Helm updates\", \"matchPaths\": [\"$dir\"], \"matchManagers\": [\"helm-requirements\", \"helm-values\", \"helmfile\", \"helmsman\", \"helmv3\"], \"groupName\": \"$dir\", \"additionalBranchPrefix\": \"\"}]")
+    sum=$(echo $sum | jq ".packageRules |= .+ [{\"description\": \"Group $dir Helm updates\", \"matchPaths\": [\"$dir\"], \"matchManagers\": [\"helm-requirements\", \"helm-values\", \"helmfile\", \"helmsman\", \"helmv3\"], \"groupName\": \"$dir\", \"additionalBranchPrefix\": \"\", \"semanticCommitScope\": \"$dir\", \"semanticCommitType\": \"{{#if isPatch}}fix{{else}}feat{{/if}}\"}]")
 
 done
 echo $sum | jq > renovate.json
