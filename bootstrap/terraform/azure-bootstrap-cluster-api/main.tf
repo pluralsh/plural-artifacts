cea40fd5d4052273c2e610300f0e345fc280b429
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-07-10 17:18:09 +0200

    Update Azure Terraform

--- bootstrap/terraform/azure-bootstrap-cluster-api/main.tf
+++ bootstrap/terraform/azure-bootstrap-cluster-api/main.tf
@@ -17,7 +17,7 @@ module "network" {
   source = "github.com/pluralsh/terraform-azurerm-network?ref=plural"
 
   vnet_name           = var.network_name
-  resource_group_name = data.azurerm_resource_group.group.name
+  resource_group_name = one(data.azurerm_resource_group.group[*].name)
   address_space       = var.address_space
   subnet_prefixes     = var.subnet_prefixes
   subnet_names        = [var.subnet_name]
@@ -29,19 +29,19 @@ module "aks" {
 
   source = "github.com/pluralsh/terraform-azurerm-aks?ref=ea5c22775e0352ef6fe7a9abe2d94306029b6a6e" # branch auto-scaler-profile
 
-  resource_group_name              = data.azurerm_resource_group.group.name
+  resource_group_name              = one(data.azurerm_resource_group.group[*].name)
   kubernetes_version               = var.kubernetes_version
   orchestrator_version             = var.kubernetes_version
   prefix                           = var.name
   cluster_name                     = var.name
   network_plugin                   = var.network_plugin
-  vnet_subnet_id                   = module.network.vnet_subnets[0]
+  vnet_subnet_id                   = one(module.network[*].vnet_subnets[0])
   os_disk_size_gb                  = var.node_groups[0].os_disk_size_gb
   os_disk_type                     = var.node_groups[0].os_disk_type
   enable_role_based_access_control = true
   rbac_aad_enabled                 = false 
   rbac_aad_managed                 = false
-  location                         = data.azurerm_resource_group.group.location
+  location                         = one(data.azurerm_resource_group.group[*].location)
   sku_tier                         = "Paid"
   private_cluster_enabled          = var.private_cluster
   enable_http_application_routing  = false
@@ -79,7 +79,7 @@ module "aks" {
 resource "azurerm_kubernetes_cluster_node_pool" "main" {
   for_each = var.migrated ? {} : {for idx, val in var.node_groups : val.name => val if idx != 0}
 
-  kubernetes_cluster_id = module.aks.aks_id
+  kubernetes_cluster_id = one(module.aks[*].aks_id)
 
   name                  = each.value.name
   priority              = each.value.priority
@@ -92,7 +92,7 @@ resource "azurerm_kubernetes_cluster_node_pool" "main" {
   max_count             = each.value.max_count
   spot_max_price        = each.value.spot_max_price
   eviction_policy       = each.value.eviction_policy
-  vnet_subnet_id        = module.network.vnet_subnets[0]
+  vnet_subnet_id        = one(module.network[*].vnet_subnets[0])
   vm_size               = each.value.vm_size
   os_disk_type          = each.value.os_disk_type
   os_disk_size_gb       = each.value.os_disk_size_gb
@@ -106,15 +106,15 @@ resource "azurerm_kubernetes_cluster_node_pool" "main" {
 data "azurerm_resource_group" "node_group" {
   count = var.migrated ? 0 : 1
 
-  name = module.aks.node_resource_group
+  name = one(module.aks[*].node_resource_group)
 }
 
 resource "azurerm_role_assignment" "aks-managed-identity" {
   count = var.migrated ? 0 : 1
 
-  scope                = data.azurerm_resource_group.group.id
+  scope                = one(data.azurerm_resource_group.group[*].id)
   role_definition_name = "Managed Identity Operator"
-  principal_id         = module.aks.kubelet_identity[0].object_id
+  principal_id         = one(module.aks[*].kubelet_identity[0].object_id)
 
   depends_on = [module.aks]
 }
@@ -122,9 +122,9 @@ resource "azurerm_role_assignment" "aks-managed-identity" {
 resource "azurerm_role_assignment" "aks-network-identity-kubelet" {
   count = var.migrated ? 0 : 1
 
-  scope                = module.network.vnet_id
+  scope                = one(module.network[*].vnet_id)
   role_definition_name = "Network Contributor"
-  principal_id         = module.aks.kubelet_identity[0].object_id
+  principal_id         = one(module.aks[*].kubelet_identity[0].object_id)
 
   depends_on = [module.aks, module.network]
 }
@@ -132,9 +132,9 @@ resource "azurerm_role_assignment" "aks-network-identity-kubelet" {
 resource "azurerm_role_assignment" "aks-network-identity-ssi" {
   count = var.migrated ? 0 : 1
 
-  scope                = module.network.vnet_id
+  scope                = one(module.network[*].vnet_id)
   role_definition_name = "Network Contributor"
-  principal_id         = module.aks.system_assigned_identity[0].principal_id
+  principal_id         = one(module.aks[*].system_assigned_identity[0].principal_id)
 
   depends_on = [module.aks, module.network]
 }
@@ -142,9 +142,9 @@ resource "azurerm_role_assignment" "aks-network-identity-ssi" {
 resource "azurerm_role_assignment" "aks-vm-contributor" {
   count = var.migrated ? 0 : 1
 
-  scope                = data.azurerm_resource_group.group.id
+  scope                = one(data.azurerm_resource_group.group[*].id)
   role_definition_name = "Virtual Machine Contributor"
-  principal_id         = module.aks.kubelet_identity[0].object_id
+  principal_id         = one(module.aks[*].kubelet_identity[0].object_id)
 
   depends_on = [module.aks]
 }
@@ -152,9 +152,9 @@ resource "azurerm_role_assignment" "aks-vm-contributor" {
 resource "azurerm_role_assignment" "aks-node-managed-identity" {
   count = var.migrated ? 0 : 1
 
-  scope                = data.azurerm_resource_group.node_group.id
+  scope                = one(data.azurerm_resource_group.node_group[*].id)
   role_definition_name = "Managed Identity Operator"
-  principal_id         = module.aks.kubelet_identity[0].object_id
+  principal_id         = one(module.aks[*].kubelet_identity[0].object_id)
 
   depends_on = [module.aks]
 }
@@ -162,9 +162,9 @@ resource "azurerm_role_assignment" "aks-node-managed-identity" {
 resource "azurerm_role_assignment" "aks-node-vm-contributor" {
   count = var.migrated ? 0 : 1
 
-  scope                = data.azurerm_resource_group.node_group.id
+  scope                = one(data.azurerm_resource_group.node_group[*].id)
   role_definition_name = "Virtual Machine Contributor"
-  principal_id         = module.aks.kubelet_identity[0].object_id
+  principal_id         = one(module.aks[*].kubelet_identity[0].object_id)
 
   depends_on = [module.aks]
 }
