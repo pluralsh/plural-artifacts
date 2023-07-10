690cb91b2a550555b97b0c1884eba53556d634a7
Sebastian Florek <s.florek91@gmail.com>
2023-07-10 11:47:43 +0200

    Fix gcp tf outputs

--- bootstrap/terraform/gcp-bootstrap-cluster-api/outputs.tf
+++ bootstrap/terraform/gcp-bootstrap-cluster-api/outputs.tf
@@ -1,6 +1,6 @@
 
 output "cluster" {
-  value = module.gke
+  value = var.migrated ? merge(one(data.google_container_cluster.cluster[*]), {ca_certificate=one(data.google_container_cluster.cluster[*]).master_auth.0.cluster_ca_certificate}) : one(module.gke[*])
   sensitive = true
 }
 
