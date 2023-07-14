8eb6b6761a7a365e43b5eccb4d1b4f2eff34fd75
Hans Rauer <h.w.rauer@googlemail.com>
2023-07-14 12:54:35 +0200

    rm sa creation from tf module

--- mlflow/terraform/gcp/main.tf
+++ mlflow/terraform/gcp/main.tf
@@ -2,22 +2,22 @@ resource "kubernetes_namespace" "mlflow" {
   metadata {
     name = var.namespace
     labels = {
-      "app.kubernetes.io/managed-by" = "plural"
-      "app.plural.sh/name" = "mlflow"
+      "app.kubernetes.io/managed-by"   = "plural"
+      "app.plural.sh/name"             = "mlflow"
       "platform.plural.sh/sync-target" = "pg"
     }
   }
 }
 
 module "mlflow-workload-identity" {
-  source     = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
-  name       = "${var.cluster_name}-${var.role_name}"
-  namespace  = var.namespace
-  project_id = var.project_id
+  source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+  name                = "${var.cluster_name}-${var.role_name}"
+  namespace           = var.namespace
+  project_id          = var.project_id
   use_existing_k8s_sa = true
-  annotate_k8s_sa = false
-  k8s_sa_name = var.serviceaccount_name
-  roles = []
+  annotate_k8s_sa     = false
+  k8s_sa_name         = var.serviceaccount_name
+  roles               = []
 }
 
 module "gcs_buckets" {
@@ -29,16 +29,16 @@ module "gcs_buckets" {
   service_account_email = module.mlflow-workload-identity.gcp_service_account_email
 }
 
-resource "kubernetes_service_account" "mlflow" {
-  metadata {
-    name      = var.serviceaccount_name
-    namespace = var.namespace
-    annotations = {
-      "iam.gke.io/gcp-service-account" = module.mlflow-workload-identity.gcp_service_account_email
-    }
-  }
-
-  depends_on = [
-    kubernetes_namespace.mlflow
-  ]
-}
+#resource "kubernetes_service_account" "mlflow" {
+#  metadata {
+#    name      = var.serviceaccount_name
+#    namespace = var.namespace
+#    annotations = {
+#      "iam.gke.io/gcp-service-account" = module.mlflow-workload-identity.gcp_service_account_email
+#    }
+#  }
+#
+#  depends_on = [
+#    kubernetes_namespace.mlflow
+#  ]
+#}
