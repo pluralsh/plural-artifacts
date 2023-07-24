3fdca5538649934a210b2e7d68242faad4ead244
Hans Rauer <h.w.rauer@googlemail.com>
2023-07-24 20:16:20 +0200

    sysbox artifact init

---
+++ sysbox/terraform/gcp/main.tf
@@ -0,0 +1,11 @@
+resource "kubernetes_namespace" "sysbox" {
+  metadata {
+    name = var.namespace
+    labels = {
+      "app.kubernetes.io/managed-by" = "plural"
+      "app.plural.sh/name" = "sysbox"
+
+    }
+  }
+}
+
---
+++ sysbox/terraform/gcp/variables.tf
@@ -0,0 +1,8 @@
+variable "namespace" {
+  type = string
+  default = "sysbox"
+}
+
+variable "cluster_name" {
+  type = string
+}
