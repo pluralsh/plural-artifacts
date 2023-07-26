c02496d76926124c7a28b7e671621dd14d3d744d
Walkoss <walkoss@pm.me>
2023-07-26 18:49:16 +0200

    Revert "fix: fix pr"
    This reverts commit 203bd850e428858b8d26122c8f2f682ffc7e63bf.

--- cube/terraform/aws/variables.tf
+++ cube/terraform/aws/variables.tf
@@ -11,6 +11,16 @@ variable "cube_bucket" {
   type = string
 }
 
+variable "cube_serviceaccount" {
+  type = string
+  default = "cube"
+}
+
+variable "role_name" {
+  type = string
+  default = "cube"
+}
+
 variable "force_destroy_bucket" {
   type        = bool
   default     = true
