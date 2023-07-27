73f8c12a693c596c1c95073536fe7b888c76c845
michaeljguarino <mguarino46@gmail.com>
2023-07-27 11:52:54 -0400

    publish cubejs to BETA
    Fixes issue with IRSA and publishes the app publicly

--- cube/terraform/aws/iam.tf
+++ cube/terraform/aws/iam.tf
@@ -5,5 +5,5 @@ module "assumable_role_cube" {
   role_name                     = "${var.cluster_name}-${var.role_name}"
   provider_url                  = replace(data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")
   role_policy_arns              = [module.s3_buckets.policy_arn]
-  oidc_fully_qualified_subjects = ["system:serviceaccount:${var.namespace}:${var.cube_serviceaccount}"]
+  oidc_subjects_with_wildcards  = ["system:serviceaccount:${var.namespace}:*"]
 }
