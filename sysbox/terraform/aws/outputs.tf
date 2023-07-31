65fc5b394258c07008f96d4ead2e1e2de4309ab6
Hans Rauer <h.w.rauer@googlemail.com>
2023-07-31 13:57:00 +0200

    output ngs, debug

---
+++ sysbox/terraform/aws/outputs.tf
@@ -0,0 +1,11 @@
+output "ng_merged" {
+  value = module.node_groups.ng_merged
+}
+
+output "ng_temp" {
+  value = module.node_groups.ng_temp
+}
+
+output "ng_expanded" {
+  value = module.node_groups.ng_expanded
+}
