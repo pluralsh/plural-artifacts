8081ea839ce3142c1666f5bae51210f0c33db50e
Hans Rauer <h.w.rauer@googlemail.com>
2023-08-01 12:50:36 +0200

    add outputs to artifact

--- sysbox/terraform/aws/outputs.tf
+++ sysbox/terraform/aws/outputs.tf
@@ -9,3 +9,19 @@ output "ng_temp" {
 output "ng_expanded" {
   value = module.node_groups.ng_expanded
 }
+
+output "user_data_kubelet_extra_args" {
+  value = module.node_groups.user_data_kubelet_extra_args
+}
+
+output "kubelet_extra_args" {
+  value = module.node_groups.kubelet_extra_args
+}
+
+output "k8s_labels" {
+  value = module.node_groups.k8s_labels
+}
+
+output "k8s_taints" {
+  value = module.node_groups.k8s_taints
+}
