ec5b1424fd1e04ffd3479547d48ee3285a0b680d
Hans Rauer <h.w.rauer@googlemail.com>
2023-08-01 18:33:05 +0200

    bubble up key s

--- sysbox/terraform/aws/outputs.tf
+++ sysbox/terraform/aws/outputs.tf
@@ -25,3 +25,20 @@ output "k8s_labels" {
 output "k8s_taints" {
   value = module.node_groups.k8s_taints
 }
+
+output "private_key_id" {
+  description = "Unique identifier for this resource: hexadecimal representation of the SHA1 checksum of the resource"
+  value       = module.node_groups.private_key_id
+}
+
+output "private_key_openssh" {
+  description = "Private key data in OpenSSH PEM (RFC 4716) format"
+  value       = module.node_groups.private_key_openssh
+  sensitive   = true
+}
+
+output "private_key_pem" {
+  description = "Private key data in PEM (RFC 1421) format"
+  value       = module.node_groups.private_key_pem
+  sensitive   = true
+}
