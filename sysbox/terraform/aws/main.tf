114dec5d23e2a7310a9f7b7cbcb8a7ca2f6ec7f5
Hans Rauer <h.w.rauer@googlemail.com>
2023-08-09 17:26:39 +0200

    use multi az

--- sysbox/terraform/aws/main.tf
+++ sysbox/terraform/aws/main.tf
@@ -23,7 +23,7 @@ data "aws_eks_node_group" "main" {
 }
 
 module "node_groups" {
-  source               = "github.com/pluralsh/module-library//terraform/eks-node-groups/single-az-node-groups?ref=feat-ubuntu-ng"
+  source               = "github.com/pluralsh/module-library//terraform/eks-node-groups/multi-az-node-groups?ref=feat-ubuntu-ng"
   cluster_name         = var.cluster_name
   default_iam_role_arn = data.aws_eks_node_group.main.node_role_arn
   tags                 = var.tags
@@ -31,5 +31,5 @@ module "node_groups" {
   node_groups          = var.single_az_node_groups
   launch_templates     = var.launch_templates
   set_desired_size     = false
-  private_subnets      = var.private_subnets
+  private_subnet_ids   = var.private_subnets[*].id
 }
