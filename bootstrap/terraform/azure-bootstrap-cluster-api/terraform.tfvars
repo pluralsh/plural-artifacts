9193fc27d6c7a39afefb58599f949ac0ea9a87da
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-09-05 09:44:02 +0200

    Remove azure-bootstrap-cluster-api Terraform

--- bootstrap/terraform/azure-bootstrap-cluster-api/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-cluster_name = {{ .Cluster | quote }}
-resource_group = {{ .Project | quote }}
--- bootstrap/terraform/azure-bootstrap-cluster-api/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "cluster_name" {
-  type = string
-  default = "plural"
-}
-
-variable "resource_group" {
-  type = string
-}
