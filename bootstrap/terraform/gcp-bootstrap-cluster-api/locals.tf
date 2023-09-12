90785902ab86f0636da3f836564903ce8157a10c
David van der Spek <vanderspek.david@gmail.com>
2023-09-12 10:33:14 +0200

    cleanup gcp-bootstrap-cluster-api
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>

--- bootstrap/terraform/gcp-bootstrap-cluster-api/locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  # required for backwards compatibility
-  gcp_region         = "${split("-", var.gcp_region)[0]}-${split("-", var.gcp_region)[1]}"
-  pods_cidr_name     = "${var.gcp_region}-${var.cluster_name}-pods"
-  services_cidr_name = "${var.gcp_region}-${var.cluster_name}-services"
-  vpc_network_name   = var.vpc_network_name != "" ? var.vpc_network_name : "${var.vpc_name_prefix}-network"
-  vpc_subnetwork_name = var.vpc_subnetwork_name != "" ? var.vpc_subnetwork_name : "${var.vpc_name_prefix}-subnetwork"
-}
